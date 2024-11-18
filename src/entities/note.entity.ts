23f380e8c8240e25360f699f55bf36f8dc34d83e
Domen Gnezda <domen.gnezda@zenlab.si>
2024-11-18 14:00:24 +0100

    Refactor Note entity: remove unnecessary semicolons and ensure consistent formatting

--- src/entities/note.entity.ts
+++ src/entities/note.entity.ts
@@ -1,18 +1,18 @@
-import { Column, Entity, ManyToOne, OneToOne } from "typeorm";
-import { Base } from "./base.entity";
-import { User } from "./user.entity";
+import { Column, Entity, ManyToOne, OneToOne } from 'typeorm'
+import { Base } from './base.entity'
+import { User } from './user.entity'
 
 @Entity()
 export class Note extends Base {
   @Column()
-  title: string;
+  title: string
 
   @Column({ type: 'text', nullable: false })
-  content: string;
+  content: string
 
   @Column({ default: false })
-  isPinned: boolean;
+  isPinned: boolean
 
   @ManyToOne(() => User, (user) => user.notes)
-  user: User;
+  user: User
 }
