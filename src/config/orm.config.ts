7624e0fc6be80ebb1cbfed8d12c5312adea28da5
Domen <domen.gnezda@pm.me>
2024-12-04 05:26:59 +0100

    Refactor ORM configuration to use DATABASE_URL

--- src/config/orm.config.ts
+++ src/config/orm.config.ts
@@ -9,11 +9,12 @@ type ConnectionOptions = ConfigType
 
 export const ORMConfig = async (configService: ConfigService): Promise<ConnectionOptions> => ({
   type: 'postgres',
-  host: configService.get('DATABASE_HOST'),
-  port: configService.get('DATABASE_PORT'),
-  username: configService.get('DATABASE_USERNAME'),
-  password: configService.get('DATABASE_PWD'),
-  database: configService.get('DATABASE_NAME'),
+  // host: configService.get('DATABASE_HOST'),
+  // port: configService.get('DATABASE_PORT'),
+  // username: configService.get('DATABASE_USERNAME'),
+  // password: configService.get('DATABASE_PWD'),
+  // database: configService.get('DATABASE_NAME'),
+  url: configService.get('DATABASE_URL'),
   // entities: ['**/*.entity.js'], // change to .ts/.js
   // entities: [User, Note],
   autoLoadEntities: true,
